2745b3a43fd91adf45e83e13dd7898b6ca53630d
Pawan Piumal <pawanpiumal1@gmail.com>
2026-01-26 22:23:08 +0530

    cc

--- src/en/NovelZlood.lua
+++ src/en/NovelZlood.lua
@@ -1,7 +1,7 @@
--- {"id":20260126,"ver":"1.0.0","libVer":"1.0.0","author":"GPPA","dep":["Madara>=2.2.0"]}
+-- {"id":260126,"ver":"1.0.0","libVer":"1.0.0","author":"GPPA","dep":["Madara>=2.2.0"]}
 
 return Require("Madara")("https://novel-zlood.github.io", {
-	id = 20260126,
+	id = 260126,
 	name = "Novel Zlood",
 	imageURL = "https://novel-zlood.github.io/images/logo.png",
 
