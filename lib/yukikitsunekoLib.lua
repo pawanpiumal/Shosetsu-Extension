cded346f3e502289f92c6d34c5cf77412a54b592
Pawan Piumal <pawanpiumal1@gmail.com>
2026-01-28 00:24:55 +0530

    c

---
+++ lib/yukikitsunekoLib.lua
@@ -0,0 +1,132 @@
+-- {"ver":"0.0.57","author":"GPPA"}
+local settings = {}
+
+local defaults = {
+    latestNovelSel = "ul.project-list",
+
+    novelPageTitleSel = "li.project-item",
+
+    hasSearch = false,
+
+    --- Some sites require custom CSS to exist, such as RTL support
+    customStyle = ""
+}
+
+function defaults:latest(data)
+    return self.parse(GETDocument(self.baseURL))
+end
+
+---@param url string
+---@return string
+function defaults:getPassage(url)
+    local htmlElement = GETDocument(self.expandURL(url)):selectFirst("article.post.h-entry")
+    local title = htmlElement:selectFirst("h1.posttitle.p-name"):text()
+    htmlElement = htmlElement:selectFirst("div.content.e-content")
+    -- Chapter title inserted before chapter text
+    -- htmlElement:prepend("<h1>" .. title .. "</h1>");
+
+    htmlElement:select("a"):remove()
+
+    return pageOfElem(htmlElement, true, self.customStyle)
+end
+
+---@param url string
+---@param loadChapters boolean
+---@return NovelInfo
+function defaults:parseNovel(url, loadChapters)
+    local doc = GETDocument(self.expandURL(url))
+
+    local content = doc:selectFirst("article.post div.content")
+
+    local h = content and content:selectFirst("h1")
+    local info = NovelInfo {
+        title = h and h:text() or ""
+    }
+
+    -- Chapters
+    -- Overrides `doc` if self.chaptersScriptLoaded is true.
+    if loadChapters and content then
+
+        -- select all paragraph elements and guard nil anchors
+        local chapterList = content:selectFirst("p"):select("a") or {}
+        local chapterOrder = 1
+
+        local novelList = AsList(mapNotNil(chapterList, function(v)
+            local a = v:selectFirst("a")
+
+            local href = a:attr("href") or ""
+            if href == "#" or href == "" then
+                return nil
+            end
+
+            return NovelChapter {
+                title = a:text() or href,
+                link = href,
+                order = chapterOrder
+            }
+        end))
+
+        info:setChapters(novelList)
+    end
+
+    return info
+end
+
+function defaults:search(data)
+    return nil
+end
+
+function defaults:expandURL(url)
+    return self.baseURL .. "/" .. url
+end
+
+function defaults:shrinkURL(url)
+    return url
+end
+
+---@param doc Document
+function defaults:parse(doc)
+    return map(doc:select("a"), function(v)
+        local a = v:attr("href")
+        -- if not a then return nil end
+
+        -- local href = a:attr("href") or ""
+        -- if href == "#" or href == "" then
+        --     return nil
+        -- end
+        -- href = self.shrinkURL(href)
+
+        -- local tit = a:text() or a:attr("title") or ""
+        -- -- trim whitespace
+        -- tit = tit:gsub("^%s*(.-)%s*$", "%1")
+        -- if tit == "" then
+        --     tit = href
+        -- end
+
+        local novel = Novel()
+        novel:setLink("href")
+
+        novel:setTitle(a)
+        return novel
+    end)
+end
+
+return function(baseURL, _self)
+    _self = setmetatable(_self or {}, {
+        __index = function(_, k)
+            local d = defaults[k]
+            return (type(d) == "function" and wrap(_self, d) or d)
+        end
+    })
+
+    _self.genres_map = {}
+    local keyID = 100
+
+    _self["baseURL"] = baseURL
+    _self["listings"] = {Listing("Default", true, _self.latest)}
+    _self["updateSetting"] = function(id, value)
+        settings[id] = value
+    end
+
+    return _self
+end
---
+++ src/en/YukiKitsuneko.lua
@@ -0,0 +1,10 @@
+-- {"id":260127,"ver":"0.0.59","libVer":"1.0.0","author":"GPPA"}
+
+
+return Require("yukikitsunekoLib")("https://yukikitsuneko.blogspot.com/", {
+	id = 260127,
+	name = "Yuki Kitsuneko",
+	imageURL = "https://blogger.googleusercontent.com/img/a/AVvXsEjmtcoZ_MDWp2CpXJZSAaSXOqqL-E54sJ4nw9AxmSv3yprfwVvxipfp41x8g2Q96eCorBx8S4vQZOZdggz_HE7k2G8MlfBFlYnRtNioP_CI7o2TbSfKIa9IVxY2xMDo1MVlUJixzeBWr__ZWgNQsoDc6Qmp0kLeTYlmZ4iHHT0yazeJL4aOsDNkUxQUMm7P=s640",
+
+
+})
